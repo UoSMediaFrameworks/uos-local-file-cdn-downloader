ba660ea7e08d076c8801b8dc1a8b1fb930e21338
aaronphillips <aaronp261@gmail.com>
2017-03-26 22:53:46 +0100

    Implemented basic bash script to complete the cdn preparation

---
+++ complete-cdn-setup.sh
@@ -0,0 +1,8 @@
+
+echo "moving files from cdn relative directory to /var/cdn for nginx static file serving"
+
+sudo cp -R ./cdn /var/cdn
+
+echo "changing permission of /var/cdn directory and contents for nginx static file serving"
+
+sudo chown -R www-data:www-data /var/cdn
