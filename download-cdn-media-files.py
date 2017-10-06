2f3e93bda7f21ee809e343ce68e087fca8d81f57
Aaron Phillips <aaronp261@gmail.com>
2017-10-06 11:59:59 +0100

    Updated the video download part of the script to gracefully handle AWS or Azure transcoded media.
    Also fix the URL encoding problem, with %20s written in file names, it breaks URL encoding.

--- download-cdn-media-files.py
+++ download-cdn-media-files.py
@@ -4,6 +4,7 @@ import requests
 import os
 from tqdm import tqdm
 import logging
+import urllib
 
 logging.basicConfig(filename='./download_errors.log', level=logging.ERROR,
                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
@@ -14,6 +15,8 @@ imageIngest = 'ingest-files/image-media-objects-for-download.json'
 
 azure_blob_url = 'https://uosassetstore.blob.core.windows.net/assetstoredev/'
 
+download_images = False
+download_videos = True
 
 def ensure_cdn_directories_are_created():
     # APEP ensure root directory is created, folders of image ids go directly in here
@@ -39,6 +42,7 @@ ensure_cdn_directories_are_created()
 def download_image_media_file_and_store(imoJson):
     print("download_image_media_file_and_store")
     image_url = imoJson['image']['url']
+    image_url = urllib.unquote(image_url)
 
     # 1. strip https://uosassetstore.blob.core.windows.net/assetstoredev/ and include cdn path
     image_file_path = 'cdn/' + image_url.replace(azure_blob_url, '')
@@ -61,10 +65,11 @@ def download_image_media_file_and_store(imoJson):
         logger.info(imoJson)
 
 
-def download_media_file(url, local_cdn_file_path):
+def download_media_file(url, local_cdn_file_path, ignore_errors=False):
     # print("Downloading media file from {} to local cdn location {}".format(url, local_cdn_file_path))
     media_file_download_r = requests.get(url, stream=True)
-    media_file_download_r.raise_for_status()
+    if ignore_errors:
+        media_file_download_r.raise_for_status()
     with open(local_cdn_file_path, 'wb') as rawOutput:
         for data in tqdm(media_file_download_r.iter_content(chunk_size=1024)):
             rawOutput.write(data)
@@ -73,6 +78,7 @@ def download_media_file(url, local_cdn_file_path):
 def download_video_media_object_and_store(vmo_json):
     print("download_video_media_object_and_store")
     raw_video_url = vmo_json['video']['url']
+    raw_video_url = urllib.unquote(raw_video_url)
 
     # 1. strip https://uosassetstore.blob.core.windows.net/assetstoredev/video/raw/
     raw_video_file_path = 'cdn/' + raw_video_url.replace(azure_blob_url, '')
@@ -93,24 +99,40 @@ def download_video_media_object_and_store(vmo_json):
 
     # 5. expand urls to include all the transcoded assets
     audio_url       = azure_blob_url + "video/transcoded/dash/" + vmo_json["_id"] + "/audio_128k.mp4"
+    audio_url_aws   = azure_blob_url + "video/transcoded/dash/" + vmo_json["_id"] + "/audio-final.webm"
     video_600k_url  = azure_blob_url + "video/transcoded/dash/" + vmo_json["_id"] + "/video_600k.mp4"
+    video_560k_url  = azure_blob_url + "video/transcoded/dash/" + vmo_json["_id"] + "/video_512x384_560k-final.webm"
     video_1200k_url = azure_blob_url + "video/transcoded/dash/" + vmo_json["_id"] + "/video_1200k.mp4"
+    video_1050k_url = azure_blob_url + "video/transcoded/dash/" + vmo_json["_id"] + "/video_640x480_1050k-final.webm"
     video_2400k_url = azure_blob_url + "video/transcoded/dash/" + vmo_json["_id"] + "/video_2400k.mp4"
+    video_2350k_url = azure_blob_url + "video/transcoded/dash/" + vmo_json["_id"] + "/video_1280x720_2350k-final.webm"
     video_4800k_url = azure_blob_url + "video/transcoded/dash/" + vmo_json["_id"] + "/video_4800k.mp4"
+    video_4300k_url = azure_blob_url + "video/transcoded/dash/" + vmo_json["_id"] + "/video_1920x1080_4300k-final.webm"
     video_manifest_url = azure_blob_url + "video/transcoded/dash/" + vmo_json["_id"] + "/video_manifest.mpd"
+
     # 5.1 generate local cdn file paths
     audio_file_path       = "cdn/video/transcoded/dash/" + vmo_json["_id"] + "/audio_128k.mp4"
+    audio_file_path_aws   = "cdn/video/transcoded/dash/" + vmo_json["_id"] + "/audio-final.webm"
     video_600k_file_path  = "cdn/video/transcoded/dash/" + vmo_json["_id"] + "/video_600k.mp4"
+    video_560k_file_path  = "cdn/video/transcoded/dash/" + vmo_json["_id"] + "/video_512x384_560k-final.webm"
     video_1200k_file_path = "cdn/video/transcoded/dash/" + vmo_json["_id"] + "/video_1200k.mp4"
+    video_1050k_file_path = "cdn/video/transcoded/dash/" + vmo_json["_id"] + "/video_640x480_1050k-final.webm"
     video_2400k_file_path = "cdn/video/transcoded/dash/" + vmo_json["_id"] + "/video_2400k.mp4"
+    video_2350k_file_path = "cdn/video/transcoded/dash/" + vmo_json["_id"] + "/video_1280x720_2350k-final.webm"
     video_4800k_file_path = "cdn/video/transcoded/dash/" + vmo_json["_id"] + "/video_4800k.mp4"
+    video_4300k_file_path = "cdn/video/transcoded/dash/" + vmo_json["_id"] + "/video_1920x1080_4300k-final.webm"
     video_manifest_file_path = "cdn/" + "video/transcoded/dash/" + vmo_json["_id"] + "/video_manifest.mpd"
 
     download_media_file(audio_url, audio_file_path)
+    download_media_file(audio_url_aws, audio_file_path_aws)
     download_media_file(video_600k_url, video_600k_file_path)
+    download_media_file(video_560k_url, video_560k_file_path)
     download_media_file(video_1200k_url, video_1200k_file_path)
+    download_media_file(video_1050k_url, video_1050k_file_path)
     download_media_file(video_2400k_url, video_2400k_file_path)
+    download_media_file(video_2350k_url, video_2350k_file_path)
     download_media_file(video_4800k_url, video_4800k_file_path)
+    download_media_file(video_4300k_url, video_4300k_file_path)
     download_media_file(video_manifest_url, video_manifest_file_path)
 
     # TODO 6. most likely going to need to handle 404s in case of missing audio
@@ -118,12 +140,14 @@ def download_video_media_object_and_store(vmo_json):
     # TODO 7. we may have an issue as not all scenes have gone through the AWS pipeline
 
 
-with open(videoIngest) as videoIngestFile:
-    videoIngestData = json.load(videoIngestFile)
-    for videoJson in videoIngestData:
-        download_video_media_object_and_store(videoJson)
+if download_videos:
+    with open(videoIngest) as videoIngestFile:
+        videoIngestData = json.load(videoIngestFile)
+        for videoJson in videoIngestData:
+            download_video_media_object_and_store(videoJson)
 
-with open(imageIngest) as imageIngestFile:
-    imageIngestData = json.load(imageIngestFile)
-    for imageJson in imageIngestData:
-        download_image_media_file_and_store(imageJson)
+if download_images:
+    with open(imageIngest) as imageIngestFile:
+        imageIngestData = json.load(imageIngestFile)
+        for imageJson in imageIngestData:
+            download_image_media_file_and_store(imageJson)
